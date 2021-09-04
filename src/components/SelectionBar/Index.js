2d77707ee654f2354efcf8d7869f69e29547498b
JamesCrowleyTech <jamescrowley467@gmail.com>
2021-09-04 18:35:57 +0100

    Added generate new grid button

--- src/components/SelectionBar/Index.js
+++ src/components/SelectionBar/Index.js
@@ -2,57 +2,58 @@ import React, { useState, useEffect, useContext } from "react";
 import "./Index.css";
 import { AppContext } from "../App/App.js";
 
-export default function SelectionBar() {
-    const findSliderValue = function (valuesToSpeed, num) {
-        if (num >= valuesToSpeed[valuesToSpeed.length - 1]) {
-            return valuesToSpeed[valuesToSpeed.length - 1];
-        }
+const findSliderValue = function (valuesToSpeed, num) {
+    if (num >= valuesToSpeed[valuesToSpeed.length - 1]) {
+        return valuesToSpeed[valuesToSpeed.length - 1];
+    }
 
-        return valuesToSpeed.findIndex(function (_, i) {
-            return valuesToSpeed[i] <= num && num < valuesToSpeed[i + 1];
-        });
-    };
-
-    const { state, dispatch } = useContext(AppContext);
-
-    const speedSliderIntervals = [
-        { 1: "4" },
-        { 2: "10" },
-        { 5: "40" },
-        { 10: "100" },
-        { 20: "200" },
-        { 50: "500" },
-        { 100: "1000" },
-        { 250: "3000" },
-        { 1000: "10000" },
-    ];
-
-    let speedSliderPoints = 0;
-    const sliderValueToSpeed = [];
-
-    speedSliderIntervals.forEach(function (pair, i) {
-        const [[n, v]] = Object.entries(pair);
-        if (i === 0) {
-            speedSliderPoints += Math.round(parseInt(v) / n);
-            for (let j = 1; j <= parseInt(v); j += parseInt(n)) {
-                sliderValueToSpeed.push(j);
-            }
-        } else {
-            // prettier-ignore
-            const prev = Object.entries(speedSliderIntervals[i - 1])
-            speedSliderPoints += (parseInt(v) - parseInt(prev[0][1])) / n;
-            for (
-                let j = parseInt(prev[0][1]) + parseInt(n);
-                j <= parseInt(v);
-                j += parseInt(n)
-            ) {
-                sliderValueToSpeed.push(j);
-            }
-        }
+    return valuesToSpeed.findIndex(function (_, i) {
+        return valuesToSpeed[i] <= num && num < valuesToSpeed[i + 1];
     });
+};
+
+const speedSliderIntervals = [
+    { 1: "4" },
+    { 2: "10" },
+    { 5: "40" },
+    { 10: "100" },
+    { 20: "200" },
+    { 50: "500" },
+    { 100: "1000" },
+    { 250: "3000" },
+    { 1000: "10000" },
+];
+
+let speedSliderPoints = 0;
+const sliderValueToSpeed = [];
+
+speedSliderIntervals.forEach(function (pair, i) {
+    const [[n, v]] = Object.entries(pair);
+    if (i === 0) {
+        speedSliderPoints += Math.round(parseInt(v) / n);
+        for (let j = 1; j <= parseInt(v); j += parseInt(n)) {
+            sliderValueToSpeed.push(j);
+        }
+    } else {
+        // prettier-ignore
+        const prev = Object.entries(speedSliderIntervals[i - 1])
+        speedSliderPoints += (parseInt(v) - parseInt(prev[0][1])) / n;
+        for (
+            let j = parseInt(prev[0][1]) + parseInt(n);
+            j <= parseInt(v);
+            j += parseInt(n)
+        ) {
+            sliderValueToSpeed.push(j);
+        }
+    }
+});
+
+export default function SelectionBar() {
+    const { state, dispatch } = useContext(AppContext);
 
     useEffect(() => {
         const speedSlider = document.getElementById("speed_slider");
+        const generateButton = document.getElementById("button-generate");
         speedSlider.value = findSliderValue(sliderValueToSpeed, 100);
         const speedSliderListener = function () {
             dispatch({
@@ -61,6 +62,12 @@ export default function SelectionBar() {
             });
         };
 
+        const generateListener = function () {
+            dispatch({ type: "NEW_GRID", payload: null });
+        };
+
+        generateButton.addEventListener("click", generateListener);
+
         speedSlider.addEventListener(
             "input",
             speedSliderListener.bind(speedSlider)
@@ -68,27 +75,20 @@ export default function SelectionBar() {
 
         return function () {
             speedSlider.removeEventListener("input", speedSliderListener);
+            generateListener.removeEventListener("click", generateListener);
         };
     }, []);
 
     return (
         <div className="selection">
             <h1 className="selection-title">Dijkstra Visualizer</h1>
-            <div className="slider-container">
-                <h2 className="slider-title">Speed:</h2>
-                <div className="slider-frame">
-                    <input
-                        type="range"
-                        id="speed_slider"
-                        name="speed_slider"
-                        min={0}
-                        max={sliderValueToSpeed.length - 1}
-                        className="slider"
-                    ></input>
-                </div>
-
-                <h2 className="slider-display">{state.speed}%</h2>
-            </div>
+            <button
+                type="button"
+                className="button-generate"
+                id="button-generate"
+            >
+                GENERATE!
+            </button>
             <div className="slider-container">
                 <h2 className="slider-title">Speed:</h2>
                 <div className="slider-frame">
--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,16 @@
 import React from "react";
+import { generateGrid } from "./helpers.js";
 
 export default function reducer(state, action) {
     if (action.type === "SET_SPEED") return { ...state, speed: action.payload };
+    if (action.type === "NEW_GRID") {
+        return {
+            ...state,
+            occupied: generateGrid(
+                state.numberOfRows,
+                state.numberOfColumns,
+                state.numberOfNodes
+            ),
+        };
+    }
 }
